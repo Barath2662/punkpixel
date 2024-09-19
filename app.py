98ebb83f58b111564732452e343bd9eef98118ad
Barath2662 <143725967+Barath2662@users.noreply.github.com>
2024-09-19 21:56:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -61,4 +61,4 @@ def ask():
     return jsonify({'answer': answer})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=False)
